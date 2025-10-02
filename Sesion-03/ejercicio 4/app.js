308a5980985f591576469d8dfb5818b73f315b4f
ferchasss <mma.fernandam@yahoo.com.mx>
2025-10-01 20:47:39 -0600

    elimine y movi carpetas

--- Sesion-03/ejercicio 4/app.js
+++ Sesion-03/ejercicio 4/app.js
@@ -16,6 +16,7 @@ ctx.lineWidth = 15;
 //ctx.ellipse (x,y, radiusx, radiusy, rotation, startAngle, endAngle);
 ctx.ellipse(100, 100, 60, 60, 0, 0, Math.PI * 2);
 ctx.stroke();
+ctx.closePath(); //cierra camino
 
 //agrupar en una funcion
 //con parametros para dibujar circulos en diferentes posiciones
@@ -27,8 +28,15 @@ function dibujarCirculo(x, y) {
     //ctx.ellipse (x,y, radiusx, radiusy, rotation, startAngle, endAngle);
     ctx.ellipse(x, y, 60, 60, 0, 0, Math.PI * 2);
     ctx.stroke();
+    ctx.closePath(); //cierra camino
 }
 
 dibujarCirculo(100, 200);
 dibujarCirculo(200, 300);   
 dibujarCirculo(300, 400);
+//escuchar el evento en la posicion del mouse
+window.addEventListener("mousemove", function (event)  {
+
+    //dibujar circulo en la posicion del mouse
+    dibujarCirculo(event.clientX, event.clientY);
+});
